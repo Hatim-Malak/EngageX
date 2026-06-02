432d352bfbe93ed4d3e5c4c4614b92690d556fe2
Hatim Malak <hatim05042006@gmail.com>
2026-06-02 23:10:28 +0530

    reconfigured cors

--- AI_Services/main.py
+++ AI_Services/main.py
@@ -32,7 +32,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(SlowAPIMiddleware)
 
 ALLOWED_ORIGINS = os.getenv(
-    "https://engage-x-rouge.vercel.app/",
+    "https://engage-x-rouge.vercel.app",
     "http://localhost:5173"
 ).split(",")
 
