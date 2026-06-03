c07d7e423f32f40a29c109da1fa9ea6aa44bb92e
Hatim Malak <hatim05042006@gmail.com>
2026-06-04 00:22:39 +0530

    fix(api): update local and production API base URLs

--- Frontend/src/store/api.js
+++ Frontend/src/store/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.MODE === "development"
-  ? 'http://localhost:8080'
-  : "https://d1rhliafz49fm8.cloudfront.net";
+  ? 'http://localhost:8000'
+  : "https://routinex-v2.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
